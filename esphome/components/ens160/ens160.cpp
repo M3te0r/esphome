016aff7d5a552b20779b8ac0c59204cfca6613e0
Mathieu Péquin <mathieu@pequin.fr>
2024-03-12 00:05:47 +0100

    Update ens160.cpp

--- esphome/components/ens160/ens160.cpp
+++ esphome/components/ens160/ens160.cpp
@@ -60,7 +60,7 @@ void ENS160Component::setup() {
   }
   ESP_LOGI(TAG, "ENS160 Part id :  %s ", part_id);
   ESP_LOGI(TAG, "BBBB");
-  ESP_LOGI(TAG, part_id);
+  ESP_LOGI(TAG, "%s",part_id);
   ESP_LOGI(TAG, "ENS160_PART_ID");
   ESP_LOGI(TAG, ENS160_PART_ID);
   
